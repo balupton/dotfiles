097d87032311915910e06e25342b0fd48cd20964
Benjamin Lupton <b@lupton.cc>
2021-01-07 06:56:44 +1100

    source.sh: update env.sh comments

--- source.sh
+++ source.sh
@@ -4,9 +4,6 @@
 # https://github.com/bevry/dorothy#configuration
 # https://github.com/bevry/dorothy/blob/master/sources/defaults.sh
 
-# load my env file
-. "$DOROTHY/user/env.sh"
-
 # dorothy ecosystem
 export GIT_PROTOCOL="ssh"
 export GIT_DEFAULT_BRANCH="main"
@@ -14,3 +11,8 @@ export GIT_DEFAULT_BRANCH="main"
 # other
 export SHELLCHECK_OPTS="-e SC2096 -e SC1090 -e SC1091 -e SC1071"
 alias go-open="open -a /Applications/GoLand.app ."
+
+# load my env file if it exists (it may be missing if it hasn't been copied over, as clones do not keep it)
+if test -f "$DOROTHY/user/env.sh"; then
+	. "$DOROTHY/user/env.sh"
+fi
