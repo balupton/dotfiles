3c500927e3b3077f3250eee9873a7aa4bd88a0c9
Benjamin Lupton <b@lupton.cc>
2021-07-06 10:11:49 +0800

    source: use ' isntead of " when " is unnecessary

--- source.sh
+++ source.sh
@@ -6,13 +6,14 @@
 # https://github.com/bevry/dorothy/blob/master/sources/defaults.sh
 
 # dorothy ecosystem
-export GIT_PROTOCOL="ssh"
-export GIT_DEFAULT_BRANCH="main"
+export GIT_PROTOCOL='ssh'
+export GIT_DEFAULT_BRANCH='main'
+export DOROTHY_THEME='oz' # 'starship'
 
 # other
 # export HOMEBREW_ARCH='arm64e'
-export SHELLCHECK_OPTS="-e SC2096 -e SC1090 -e SC1091 -e SC1071"
-alias go-open="open -a /Applications/GoLand.app ."
+export SHELLCHECK_OPTS='-e SC2096 -e SC1090 -e SC1091 -e SC1071'
+alias go-open='open -a /Applications/GoLand.app .'
 
 # source my env file
 . "$DOROTHY/user/env.sh"
