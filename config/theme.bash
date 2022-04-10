ebd9c74a5b108d2188213e03352b62a3d12652df
Benjamin Lupton <b@lupton.cc>
2022-04-11 00:37:28 +0800

    add new `theme.bash` configuration

---
+++ config/theme.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+# do not use `export` keyword in this file
+
+# Used by `macos-theme`
+
+WALLPAPER_DIRECTORY_LIGHT="$HOME/Sync/Wallpapers/Light"
+WALLPAPER_DIRECTORY_DARK="$HOME/Sync/Wallpapers/Dark"
+
+VSCODE_THEME_LIGHT='Noctis Lux'
+VSCODE_THEME_DARK='Popping and Locking'
