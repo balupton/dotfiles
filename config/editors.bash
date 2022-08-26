8614da39d8f75b1df42c34ea9f626c1d18efda59
Benjamin Lupton <b@lupton.cc>
2022-08-26 10:29:44 +0800

    editors: default gui editors to dorothy defaults

--- config/editors.bash
+++ config/editors.bash
@@ -2,11 +2,10 @@
 # do not use `export` keyword in this file:
 # shellcheck disable=SC2034
 
+# load the dorothy defaults
+source "$DOROTHY/config/editors.bash"
+
 TERMINAL_EDITORS=(
 	'vim'
 	'vi'
-)
-GUI_EDITORS=(
-	'code'
-	'gedit'
 )
