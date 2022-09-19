baaf0645f14d70c016169539faf89c19b5108d07
Benjamin Lupton <b@lupton.cc>
2022-09-20 02:32:24 +0800

    setup: add default templates to enable autocomplete

---
+++ config/shell.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# do whatever you want in this file:
+# shellcheck disable=SC2034
+
+# Used by `shell.sh`
+
+# Source the default bash shell config
+source "$DOROTHY/config/shell.bash"
---
+++ config/shell.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# do whatever you want in this file:
+# shellcheck disable=SC2034
+
+# Used by `shell.sh`
+
+# Source the default zsh shell config
+source "$DOROTHY/config/shell.zsh"
