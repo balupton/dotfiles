e4f1ebc33add0ff62b00d1cf5c5ca08e43e22314
Benjamin Lupton <b@lupton.cc>
2023-07-27 22:22:28 +0800

    autocomplete compat with https://github.com/bevry/dorothy/commit/61c49cb345a07204fd47dd3997871c91060cc892

---
+++ config/autocomplete.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+# shellcheck disable=SC2034
+# use inline `export VAR=...` statements, for fish compatibility`
+
+# Unless no, the autocompletion will be loaded if applicable
+export AUTOCOMPLETE_TEA='no'
--- config/interactive.zsh
+++
@@ -1,5 +0,0 @@
-fz() {
-  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
-}
-
-source "$DOROTHY/user/config/interactive.sh"
