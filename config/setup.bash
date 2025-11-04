4b6a599ae464f28d0c266d8da9f61df0b0df8484
Benjamin Lupton <b@lupton.cc>
2025-11-04 09:22:35 +0800

    adjust for recent preferences and dorothy changes

--- config/setup.bash
+++ config/setup.bash
@@ -9,11 +9,6 @@ source "$DOROTHY/config/setup.bash"
 hostname="$(get-hostname)"
 hostname="${hostname%.local}"
 
-# note on youtube-dl
-# brew maintains it actively, and also will assist with dependencies
-# however, ubuntu is slow to maintain, so install via pip instead
-# http://ytdl-org.github.io/youtube-dl/download.html
-
 APK_INSTALL=()
 
 # APT_UNINSTALL=(
@@ -28,9 +23,6 @@ APK_INSTALL=()
 # 	shotwell
 # 	thunderbird
 # )
-
-# software-properties-common
-# 	`atomicparsley` is a youtube-dl dependency
 APT_INSTALL=(
 	'gnome-keyring'
 	'libnotify-bin'
@@ -38,24 +30,20 @@ APT_INSTALL=(
 )
 
 # Snap
-# https://code.visualstudio.com/docs/setup/linux
-# if [[ "$(get-arch)" == 'x'* ]]; then
 SNAP_INSTALL=()
 
 # Go
 GO_LINTING_INSTALL='no'
+# - 'github.com/cloudflare/utahfs/cmd/utahfs-client'
 GO_INSTALL=()
-# if is-mac; then
-# 	GO_LINTING_INSTALL='yes'
-# 	GO_INSTALL+=(
-# 	)
-# fi
-# 'github.com/cloudflare/utahfs/cmd/utahfs-client'
 
 # macOS apps / mas
 # https://github.com/mas-cli/mas
 # '1472777122' 'Honey'
 # '1484348796' 'Endel'
+# '409183694' 'Keynote'
+# '409201541' 'Pages'
+# '409203825' 'Numbers'
 # '430798174' 'HazeOver'
 # '441258766' 'Magnet'
 # '441258766' 'Magnet'
@@ -68,9 +56,6 @@ GO_INSTALL=()
 MAS_INSTALL=(
 	'420212497' 'Byword'
 	'405399194' 'Kindle'
-	'409183694' 'Keynote'
-	'409203825' 'Numbers'
-	'409201541' 'Pages'
 )
 
 # Homebrew
@@ -85,32 +70,31 @@ HOMEBREW_FORMULAS=(
 	watchman
 )
 HOMEBREW_SLOW_FORMULAS=(
-	'shellcheck'
+	shellcheck
 )
-# 'adguard'
-# 'aerial'
-# 'calibre'
-# 'firefox'
-# 'multipass'
-# 'pocket-casts'
-# 'raspberry-pi-imager'
-# 'spotify'
-# 'twitch'
-# 'zoom'
-HOMEBREW_CASKS=(
-	'audio-hijack'
-	'loopback'
-	'soundsource'
-)
-# 'font-dejavu-sans-mono-nerd-font'
-# 'font-fira-code-nerd-font'
-# 'font-fira-mono-nerd-font'
-# 'font-inconsolata-go-nerd-font'
-# 'font-iosevka-nerd-font'
-# 'font-jetbrains-mono-nerd-font'
-# 'font-ubuntu-mono-nerd-font'
-# 'font-ubuntu-nerd-font'
-# 'font-victor-mono-nerd-font'
+# - adguard
+# - aerial
+# - audio-hijack
+# - calibre
+# - firefox
+# - loopback
+# - multipass
+# - pocket-casts
+# - raspberry-pi-imager
+# - soundsource
+# - spotify
+# - twitch
+# - zoom
+HOMEBREW_CASKS=()
+# - font-dejavu-sans-mono-nerd-font
+# - font-fira-code-nerd-font
+# - font-fira-mono-nerd-font
+# - font-inconsolata-go-nerd-font
+# - font-iosevka-nerd-font
+# - font-jetbrains-mono-nerd-font
+# - font-ubuntu-mono-nerd-font
+# - font-ubuntu-nerd-font
+# - font-victor-mono-nerd-font
 HOMEBREW_FONTS=(
 	'font-cantarell'
 	'font-cascadia-code'
@@ -141,52 +125,51 @@ HOMEBREW_FONTS=(
 )
 
 # Ruby
-# compass
-# ffi
-# sass
-# twurl
-# git-up, deprecated, fails to install on macos, replaced by `git-up` alias
+# - compass
+# - ffi
+# - sass
+# - twurl
+# - git-up # deprecated, fails to install on macos, replaced by `git-up` alias
 GEM_INSTALL=()
 
 # Python
-# @todo swap PyPDF2 for one of the following
+# - bitcoinlib
+# - cairosvg
+# - katcr
+# - poetry
+# - PyPDF2
+# - stig
+# @todo swap PyPDF2 for one of the following:
 # > https://stackoverflow.com/a/63557138/130638
 # - https://github.com/pymupdf/PyMuPDF
 # - https://github.com/pikepdf/pikepdf
 # - https://github.com/pdfminer/pdfminer.six
-# pipx:
-# 'youtube_dl'
-# 'katcr'
-# 'poetry'
-# 'stig'
-PIPX_INSTALL=()
-# PYTHON3_PIP_INSTALL=('bitcoinlib' 'cairosvg')
-# PYTHON2_PIP_INSTALL=('PyPDF2')
+UV_INSTALL=()
 
 # Node.js
 # SERVERS
-# browser-refresh
-# live-server
+# - browser-refresh
+# - live-server
 # DATABASE
-# fauna-shell
+# - fauna-shell
 # ECOSYSTEMS
-# "@stencil/core"
-# @cloudflare/wrangler
-# apollo
-# ember-cli
-# firebase-tools
-# ionic
-# lasso-cli
-# marko-cli
-# marko-starter
-# netlify-cli
-# now
+# - @stencil/core
+# - @cloudflare/wrangler
+# - apollo
+# - ember-cli
+# - firebase-tools
+# - ionic
+# - lasso-cli
+# - marko-cli
+# - marko-starter
+# - netlify-cli
+# - now
 # TOOLING
-# tldr # replaced with tealdeer
-# typescript@next
+# - tldr # replaced with tealdeer
+# - typescript@next
 # CRYPTOCURRENCY
-# cartera
-# coinmon
+# - cartera
+# - coinmon
 NPM_INSTALL=(
 	'@bevry/testen'
 	'@bevry/github-api'
